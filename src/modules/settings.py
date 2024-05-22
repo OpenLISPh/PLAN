aadd9a4b57bf3895143457075b4db7afb748ec82
Luke Hernandez <xaiha27@gmail.com>
2024-05-22 18:09:26 +0800

    Fix env_path

--- src/modules/settings.py
+++ src/modules/settings.py
@@ -2,8 +2,8 @@ import os
 
 from dotenv import load_dotenv
 
-# Path to .env file
-env_path = os.path.join(os.path.dirname(__file__), ".env")
+PROJECT_ROOT = os.getcwd()
+env_path = os.path.join(PROJECT_ROOT, ".env")
 
 # Check if .env file exists
 if not os.path.exists(env_path):
